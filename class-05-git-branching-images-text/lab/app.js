929ff8f93bc0fca22e044e36ee15197283d41e40
Srinivasa Datla <sdatla@amazon.com>
2019-03-07 23:03:06 -0800

    Added code for arrayMultiplication function

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -129,10 +129,15 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1, 2, 3, 4, 5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+    var sumMultipleResults=1;
+    for(var i=0; i < dynamicArray.length; i++) {
+        sumMultipleResults=(multiply(dynamicArray[i],sumMultipleResults)[0]);
+    }
+    console.log(sumMultipleResults);
+    return ([sumMultipleResults,'The numbers 1,2,3,4,5 have a product of 120.']);
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+ testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
